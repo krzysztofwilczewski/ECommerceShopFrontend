da2736342b18bfc6c96dc98a7d82b65d69c2ec92
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-10-31 12:08:11 +0100

    default layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
