970313236f3e56047a065a1ed6a9d96e6f946426
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-11-03 15:11:03 +0100

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
 import { FullpageModule } from './layouts/fullpage/fullpage.module';
 import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.modul
     FullpageModule,
     FullpageadminModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
